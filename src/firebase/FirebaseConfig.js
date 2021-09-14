95aad82926cf56401ddcb03d02b6020ad2932634
angiecortesdev <angiecortes29@gmail.com>
2021-09-14 10:48:27 -0500

    Se creo rama firebase

---
+++ src/firebase/FirebaseConfig.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCE_ppoYK55IzQzZh94vAOisbL4NUUXC_o",
+
+  authDomain: "talentfest-mensajeros-urbanos.firebaseapp.com",
+
+  projectId: "talentfest-mensajeros-urbanos",
+
+  storageBucket: "talentfest-mensajeros-urbanos.appspot.com",
+
+  messagingSenderId: "1051607467040",
+
+  appId: "1:1051607467040:web:7d3365e8226d141b722845"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
