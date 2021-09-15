38c5c4d858853887132c49a0d0ac63d20ac9000a
gloriacuba <gloriajiss@gmail.com>
2021-09-15 09:31:38 -0500

    Visualización de coordenadas, datos obtenidos en firestore

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -3,10 +3,17 @@ export function getPoligono() {
 db.collection("Zonas").onSnapshot((querySnapshot) => {
     let data = [];
     querySnapshot.forEach((doc) => {
-        data.push({...doc.data().poligono})
-        
+        doc.data().poligono.forEach((coordenada)=>{
+        /*data.push(`${doc.data().poligono[0].latitude},${doc.data().poligono[0].longitude}`);*/
+        let cord =[];
+        let latitude = coordenada.latitude;
+        let longitude = coordenada.longitude;
+        cord.push(latitude);
+        cord.push(longitude);
+        data.push(cord)
+    })
     });
-    console.log(data) 
+   console.log(data)
 });
 }
 
