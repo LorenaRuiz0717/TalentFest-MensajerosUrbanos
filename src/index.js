c853292c28eedb5c41d170f8132f7149059042a5
unknown <lorena0717@hotmail.com>
2021-09-16 09:22:46 -0500

    diseño login

--- src/index.js
+++ src/index.js
@@ -5,32 +5,3 @@ import App from './App';
 
 ReactDOM.render(<App/>,  document.getElementById('root'))
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import 'mapbox-gl/dist/mapbox-gl.css';
-// import './index.css';
-// import Mapas from './mapas';
-// // import reportWebVitals from './reportWebVitals';
-// import './firebase/firebaseConfig'
-
-// import SignIn from './loginIn'
- 
-// ReactDOM.render(
-  
-// <React.StrictMode>
-// <SignIn/>
-// <Mapas />
-// </React.StrictMode>,
-// document.getElementById('root')
-// );
-
-
-// return (
-//   <Router>
-//       <Route exact  path='/' component={login} />
-//       <PrivateRoute path='/Notes' component={Notes} />
-//       <PrivateRoute path='/About' component={About} /> 
-//   </Router>
-
-// );
--- src/loginIn.js
+++ src/loginIn.js
@@ -3,30 +3,31 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 // import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import fondo from './assets/fondo.png'
-import Vector from './assets/Vector.png'
+import logotype from './assets/logotype.png'
+import './formulario.css';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+// function Copyright(props) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://material-ui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
 
 const theme = createTheme();
 
@@ -42,58 +43,59 @@ export default function SignIn() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          {/*<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+    <div className='container'>
+      <div className='formulario'>
+        <ThemeProvider theme={theme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              {/*<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             {/* <LockOutlinedIcon /> 
           </Avatar>*/}
-          <Typography component="h1" variant="h5">
-          <img src={Vector} alt="vector" className='vector'  />
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
+              <Typography component="h1" variant="h5">
+                Iniciar Sesion
+              </Typography>
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 8 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
+            /> */}
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 5, mb: 2 }}
+                >
+                Iniciar Sesion
+                </Button>
+                {/* <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
@@ -104,15 +106,22 @@ export default function SignIn() {
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
-            </Grid>
-        
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-       </Container>
-       <img src={fondo} alt="gmail" className='gmail'  />
-    </ThemeProvider>
-  
+            </Grid> */}
+
+              </Box>
+            </Box>
+            {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+          </Container>
+
+        </ThemeProvider>
+      </div>
+      <div className='logo'>
+        <img src={logotype} alt="logotype" width='380px' />
+
+        <img src={fondo} alt="gmail" width='600px' />
+      </div>
+    </div>
+
   );
 }
 
